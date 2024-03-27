78e51aec199359bdb17c025b1ef3b9e3cb5527c8
fatihruffgames <164330349+fatihruffgames@users.noreply.github.com>
2024-03-27 17:23:53 +0300

    .

--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 
@@ -11,6 +9,8 @@ public class CardController : MonoBehaviour
 {
     public ColorEnum color;
     public Transform mesh;
+
+    public bool IsPicked;
 
     private void Awake()
     {
@@ -32,4 +32,12 @@ public class CardController : MonoBehaviour
                 break;
         }
     }
+
+
+
+    public void GetPicked()
+    {
+
+    }
+
 }
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,6 +4,34 @@ using UnityEngine;
 
 public class InputManager : MonoSingleton<InputManager>
 {
-     
-     
+    public LayerMask CardLayer;
+    [SerializeField] bool blockPicking;
+    public GameObject firstSelected;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 300, CardLayer))
+            {
+                if (hit.collider.TryGetComponent(out CardController card))
+                {
+                    if (blockPicking) return;
+                    if (card.IsPicked) return;
+
+                    firstSelected = card.gameObject;
+                    card.GetPicked();
+
+                    blockPicking = true;
+                }
+            }
+        }
+    }
+
+
+
+
 }
